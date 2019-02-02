1421c9056965ca0ef1b2a5686ef1255bf30fad20
Bhanu Teja <pbteja1998@gmail.com>
2019-02-03 03:41:15 +0530

    add story of Img

--- stories/index.js
+++ stories/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Button } from '@storybook/react/demo';
+import Img from "../packages/ui.img/src";
 
-storiesOf('Button', module)
-    .add('with text', () => (
-        <Button>Hello Button</Button>
-    ))
-    .add('with some emoji', () => (
-        <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
+storiesOf('Img', module)
+    .add('with src and alt', () => (
+        <Img src="https://via.placeholder.com/350x150" alt="sample imag"/>
     ));
