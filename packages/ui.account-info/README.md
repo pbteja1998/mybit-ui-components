27b1f6a01936f1b2c428eab11eb1c4b646a76971
Bhanu Teja <pbteja1998@gmail.com>
2019-02-10 14:19:36 +0530

    add account-info package

---
+++ packages/ui.account-info/README.md
@@ -0,0 +1,11 @@
+# `@pbteja-dev/ui.account-info`
+
+> TODO: description
+
+## Usage
+
+```
+const uiAccountInfo = require('@pbteja-dev/ui.account-info');
+
+// TODO: DEMONSTRATE API
+```
--- stories/index.js
+++ stories/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Img from "../packages/ui.img/src";
 import Address from "../packages/ui.address/src";
+import AccountInfo from "../packages/ui.account-info/src";
 
 storiesOf('Img', module)
     .add('with src and alt', () => (
@@ -17,16 +18,16 @@ storiesOf('Address', module)
     ))
 ;
 
-// storiesOf('Account Info', module)
-//     .add('with myBitBalance, ethBalance, userName', () => (
-//         <AccountInfo myBitBalance="20" ethBalance="8" userName="0xDA2F8123" />
-//     ))
-//     .add('without userName', () => (
-//         <AccountInfo myBitBalance="20" ethBalance="8" />
-//     ))
-//     .add('without ethBalance', () => (
-//         <AccountInfo myBitBalance="20" userName="0xDA2F8123" />
-//     ))
-//     .add('without any props', () => (
-//         <AccountInfo />
-//     ));
+storiesOf('Account Info', module)
+    .add('with myBitBalance, ethBalance, userName', () => (
+        <AccountInfo myBitBalance="20" ethBalance="8" userName="0xDA2F8123" />
+    ))
+    .add('without userName', () => (
+        <AccountInfo myBitBalance="20" ethBalance="8" />
+    ))
+    .add('without ethBalance', () => (
+        <AccountInfo myBitBalance="20" userName="0xDA2F8123" />
+    ))
+    .add('without any props', () => (
+        <AccountInfo />
+    ));
